55a35a7cf6407aab2fb3fbf34618829ba49fbbd0
friendswithbenny <friendswithbenny@gmail.com>
2016-03-01 09:28:20 -0800

    EnumUtil
    and tests

---
+++ src/main/java/org/fwb/enums/EnumUtil.java
@@ -0,0 +1,63 @@
+package org.fwb.enums;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
+import com.google.common.base.Function;
+
+public class EnumUtil {
+	/** @deprecated static utilities only */
+	@Deprecated
+	private EnumUtil () { }
+	
+	public static final Function<Enum<?>, String> ENUM_NAME = new Function<Enum<?>, String>() {
+		@Override
+		public String apply(Enum<?> e) {
+			return e.name();
+		}
+	};
+	
+	public static class EnumName<T extends Enum<T>> implements Function<T, String> {
+		@Override
+		public String apply(T t) {
+			return t.name();
+		}
+	}
+	
+	public static final Function<Class<? extends Enum<?>>, EnumSet<? extends Enum<?>>> ENUM_VALUES =
+			new Function<Class<? extends Enum<?>>, EnumSet<? extends Enum<?>>>() {
+				@Override
+				public EnumSet<? extends Enum<?>> apply(Class<? extends Enum<?>> c) {
+					@SuppressWarnings({"rawtypes", "unchecked"})
+					Class<Enum> c2 = (Class<Enum>) c;
+					
+					@SuppressWarnings("unchecked")
+					EnumSet<? extends Enum<?>> retVal = EnumSet.allOf(c2);
+					
+					return retVal;
+				}
+			};
+	
+	public static class EnumValues<T extends Enum<T>> implements Function<Class<T>, EnumSet<T>> {
+		@Override
+		public EnumSet<T> apply(Class<T> c) {
+			return EnumSet.allOf(c);
+		}
+	}
+	
+	public static final Function<Class<? extends Enum<?>>, List<? extends Enum<?>>> ENUM_VALUE_LIST =
+			new Function<Class<? extends Enum<?>>, List<? extends Enum<?>>>() {
+				@Override
+				public List<? extends Enum<?>> apply(Class<? extends Enum<?>> c) {
+					return Arrays.asList(c.getEnumConstants());
+				}
+			};
+	
+	public static class EnumValueList<T extends Enum<T>> implements Function<Class<T>, List<T>> {
+		@Override
+		public List<T> apply(Class<T> c) {
+			return Arrays.asList(c.getEnumConstants());
+		}
+	}
+}
---
+++ src/test/java/test/fwb/enums/EnumUtilTest.java
@@ -0,0 +1,81 @@
+package test.fwb.enums;
+
+import org.fwb.enums.EnumUtil;
+import org.fwb.enums.EnumUtil.EnumName;
+import org.fwb.enums.EnumUtil.EnumValueList;
+import org.fwb.enums.EnumUtil.EnumValues;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class EnumUtilTest {
+	@Test
+	public void testENUM_NAME() {
+		assertEquals(
+				Arrays.asList("a1", "b2"),
+				Lists.transform(
+						Arrays.asList(EnumA.a1, EnumB.b2),
+						EnumUtil.ENUM_NAME));
+	}
+	
+	@Test
+	public void testEnumName() {
+		assertEquals(
+				Arrays.asList("a1", "a2", "a3"),
+				Lists.transform(
+						Arrays.asList(EnumA.values()),
+						new EnumName<EnumA>()));
+	}
+	
+	@Test
+	public void testENUM_VALUES() {
+		assertEquals(
+				Arrays.asList(
+						new HashSet<Enum<?>>(Arrays.asList(EnumB.values())),
+						new HashSet<Enum<?>>(Arrays.asList(EnumA.values()))),
+				Lists.transform(
+						Arrays.asList(EnumB.class, EnumA.class),
+						EnumUtil.ENUM_VALUES));
+	}
+	
+	@Test
+	public void testEnumValues() {
+		assertEquals(
+				Arrays.asList(
+						new HashSet<Enum<?>>(Arrays.asList(EnumB.values())),
+						new HashSet<Enum<?>>(Arrays.asList(EnumB.values()))),
+				Lists.transform(
+						Arrays.asList(EnumB.class, EnumB.class),
+						new EnumValues<EnumB>()));
+	}
+	
+	@Test
+	public void testENUM_VALUE_LIST() {
+		assertEquals(
+				Arrays.asList(
+						Arrays.asList(EnumB.values()),
+						Arrays.asList(EnumA.values())),
+				Lists.transform(
+						Arrays.asList(EnumB.class, EnumA.class),
+						EnumUtil.ENUM_VALUE_LIST));
+	}
+	
+	@Test
+	public void testEnumValueList() {
+		assertEquals(
+				Arrays.asList(
+						Arrays.asList(EnumB.values()),
+						Arrays.asList(EnumB.values())),
+				Lists.transform(
+						Arrays.asList(EnumB.class, EnumB.class),
+						new EnumValueList<EnumB>()));
+	}
+	
+	enum EnumA { a1, a2, a3 }
+	enum EnumB { b3, b2, b1 }
+}
