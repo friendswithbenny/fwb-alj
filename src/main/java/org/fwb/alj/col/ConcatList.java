051d2114f8c04baaf71e989f9a7f6dc0db5de9bf
friendswithbenny <friendswithbenny@gmail.com>
2016-03-17 07:19:40 -0700

    ConcatList cleanup
    performance and docs

--- src/main/java/org/fwb/alj/col/ConcatList.java
+++ src/main/java/org/fwb/alj/col/ConcatList.java
@@ -2,14 +2,19 @@ package org.fwb.alj.col;
 
 import java.util.AbstractList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 /**
  * returns (an immutable, live-view of) the concatenation of a List of Lists.
+ * the {@link #get(int)} and {@link size()} operations are O(m)
+ * where m is the number of Lists being concatenated,
+ * but the {@link Iterator#next()} method is O(1).
  */
 public class ConcatList<T> extends AbstractList<T> {
 	final List<? extends List<? extends T>> LISTS;
@@ -17,7 +22,6 @@ public class ConcatList<T> extends AbstractList<T> {
 		LISTS = Preconditions.checkNotNull(lists);
 	}
 	
-	/** O(m) */
 	@Override
 	public T get(int index) {
 		if (0 > index)
@@ -38,10 +42,18 @@ public class ConcatList<T> extends AbstractList<T> {
 				String.format("invalid index %s for size %s", index, size()));
 	}
 	
-	/** O(m) */
 	@Override
 	public int size() {
 		return sumInts(Lists.transform(LISTS, SIZE));
+	}
+	
+	/*
+	 * a performance-enhanced over-ride which directly concatenates the iterators,
+	 * rather than AbstractList's iterator which will repeatedly call the O(m) #get method.
+	 */
+	@Override
+	public Iterator<T> iterator() {
+		return Iterables.concat(LISTS).iterator();
 	}
 	
 	static final Function<Collection<?>, Integer> SIZE =
