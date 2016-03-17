48cdcf6fc1019d40578a9c3464682e7216b6c2c0
friendswithbenny <friendswithbenny@gmail.com>
2016-03-17 10:00:53 -0700

    ListUtil.AsList and ListUtil.asList1Deep

---
+++ src/main/java/org/fwb/alj/col/ListUtil.java
@@ -0,0 +1,22 @@
+package org.fwb.alj.col;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+public class ListUtil {
+	public static <T> List<List<T>> asList1Deep(T[][] array) {
+		return Lists.transform(
+				Arrays.asList(array),
+				new AsList<T>());
+	}
+	
+	public static class AsList<T> implements Function<T[], List<T>> {
+		@Override
+		public List<T> apply(T[] input) {
+			return Arrays.asList(input);
+		}
+	}
+}
