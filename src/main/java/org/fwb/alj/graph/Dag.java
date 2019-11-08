6a62595a3102b75964cbde95d5acbe701745b59c
Benny <benny@friendswithbenny.org>
2019-11-08 02:22:51 -0800

    think i got some of dag done
    i'll kill some ghosts,
    then there are plenty of operations to shore up

--- src/main/java/org/fwb/alj/graph/Dag.java
+++ src/main/java/org/fwb/alj/graph/Dag.java
@@ -1,9 +1,10 @@
-package org.fwb.alj.dag;
+package org.fwb.alj.graph;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import com.google.common.base.Preconditions;
@@ -12,6 +13,7 @@ public class Dag<T> {
 	final Map<T, Node>
 		nodes = new HashMap<T, Node>(),
 		roots = new HashMap<T, Node>();
+//		leaves = new HashMap<T, Node>();
 	
 	final Set<Edge> edges = new HashSet<Edge>();
 //	final Map<T, Edge>
@@ -20,8 +22,13 @@ public class Dag<T> {
 	
 	public Set<T> getAncestors(T value) {
 		Set<T> ancestors = new HashSet<T>();
-		Node node = nodes.get(value);
-		
+		getAncestorsRecursive(nodes.get(value), ancestors);
+		return ancestors;
+	}
+	private void getAncestorsRecursive(Node node, Set<T> ancestors) {
+		if (ancestors.add(node.value))
+			for (Edge e : node.incoming)
+				getAncestorsRecursive(e.from, ancestors);
 	}
 	
 	/**
@@ -38,10 +45,6 @@ public class Dag<T> {
 	}
 	
 	public Edge addEdge(T from, T to) {
-		// this implementation does not allow multi-edges
-		Edge edge = new Edge(from, to);
-		Preconditions.checkState(! edges.contains(edge));
-		
 		Node
 			fromNode = nodes.get(from),
 			toNode = nodes.get(to);
@@ -51,49 +54,104 @@ public class Dag<T> {
 //		Preconditions.checkState(nodes.containsKey(from));
 //		Preconditions.checkState(nodes.containsKey(to));
 		
-		// first validate
+		// this implementation does not allow multi-edges
+		Edge edge = new Edge(fromNode, toNode);
+		Preconditions.checkState(! edges.contains(edge));
 		
+		// first validate
+		Preconditions.checkState(! getAncestors(from).contains(to));
 		
 		// then mutate.
 		edges.add(edge);
 //		sources.put(from, edge);
 //		targets.put(to, edge);
-		fromNode.outgoing.put(to, edge);
-		toNode.incoming.put(from,  edge);
+		fromNode.outgoing.add(edge);
+		toNode.incoming.add( edge);
 		
+//		leaves.remove(from);
 		roots.remove(to);
 		
 		return edge;
 	}
 	
+	public Edge removeEdge(T from, T to) {
+		Node
+			fromNode = nodes.get(from),
+			toNode = nodes.get(to);
+		Preconditions.checkNotNull(fromNode);
+		Preconditions.checkNotNull(toNode);
+		
+		Edge edge = new Edge(fromNode, toNode);
+		Preconditions.checkNotNull(edges.remove(edge));
+		
+		fromNode.outgoing.remove(edge);
+		toNode.incoming.remove(edge);
+		
+		if (toNode.incoming.isEmpty())
+			roots.put(toNode.value, toNode);
+		
+		return edge;
+	}
+	
+	public Node removeNode(T value) {
+		Node node = nodes.remove(value);
+		Preconditions.checkNotNull(node);
+		
+		roots.remove(value);
+		
+		for (Edge e: node.outgoing)
+			removeEdge(value, e.to.value);
+		for (Edge e: node.incoming)
+			removeEdge(e.from.value, value);
+		
+		return node;
+	}
+	
 	public class Node {
 		final T value;
 		
-		final Map<T, Edge>
-			incoming = new HashMap<T, Edge>(),
-			outgoing = new HashMap<T, Edge>();
+		final Set<Edge>
+			outgoing = new HashSet<Edge>(),	
+			incoming = new HashSet<Edge>();
 		
 		Node(T value) {
 			this.value = value;
 		}
 		
-		Set<Node> getParents() {
-			
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(value);
 		}
-		Set<Node> getChildren() {
-			
+		@Override
+		public boolean equals(Object o) {
+			return (o instanceof Dag.Node)
+				&& Objects.equals(value, ((Dag.Node) o).value);
 		}
+		
+//		/** an outgoing edge */
+//		public class Edge {
+//			final Node from = Node.this;
+//			final Node to;
+//			Edge(Node to) {
+//				this.to = to;
+//			}
+//		}
 	}
 	public class Edge {
 		/** special reference to help with equality testing */
 		final Dag<T> dag = Dag.this;
 		
-		final T
+		final Node
 			from,
 			to;
-		Edge(T from, T to) {
+		Edge(Node from, Node to) {
 			this.from = from;
 			this.to = to;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Arrays.asList(from, to).hashCode();
 		}
 		
 		@Override
@@ -110,13 +168,5 @@ public class Dag<T> {
 			} else
 				return false;
 		}
-		
-		@Override
-		public int hashCode() {
-			return Arrays.asList(from, to).hashCode();
-		}
-	}
-	public class Path {
-		
 	}
 }
