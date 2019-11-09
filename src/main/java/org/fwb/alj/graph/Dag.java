fe38782ac14099073d2dc79b8c339295bd6e6b1d
Benny <benny@friendswithbenny.org>
2019-11-09 15:38:58 -0800

    changed dag to publish node objects

--- src/main/java/org/fwb/alj/graph/Dag.java
+++ src/main/java/org/fwb/alj/graph/Dag.java
@@ -6,28 +6,50 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 
 import com.google.common.base.Preconditions;
+
+//import org.fwb.alj.col.SetUtil.SetView;
+//import com.google.common.collect.Collections2;
 
 public class Dag<T> {
 	private final Map<T, Node> nodes = new HashMap<T, Node>();
 	
 	private final Set<Node> roots = new HashSet<Node>();
 	
-	public Set<T> getAncestors(T value) {
-		Set<T> ancestors = new HashSet<T>();
+	public Node getNode(T value) {
+		Node node = nodes.get(value);
+		Preconditions.checkArgument(
+			null != node,
+			"node not found: %s",
+			value);
+		return node;
+	}
+	
+	/** depth-first */
+	public Set<Node> getAncestors(T value) {
+		Set<Node> ancestors = new HashSet<Node>();
 		getAncestorsRecursive(
 			getNode(value),
 			ancestors);
 		return ancestors;
 	}
-	public Set<T> getDescendants(T value) {
-		Set<T> descendants = new HashSet<T>();
+	/** depth first */
+	public Set<Node> getDescendants(T value) {
+		Set<Node> descendants = new HashSet<Node>();
 		getDescendantsRecursive(
 			getNode(value),
 			descendants);
 		return descendants;
 	}
+//	public Set<T> getAncestorValues(T value) {
+//		/* TODO this doesn't work for some reason :( */
+//		return new SetView<T>(Collections2.<Node, T>transform(getDescendants(value), getNodeValue));
+//	}
+//	public Set<T> getDescendantValues(T value) {
+//		
+//	}
 	
 	/**
 	 * add a value not already in the Dag.
@@ -59,9 +81,9 @@ public class Dag<T> {
 			from,
 			to);
 		
-		Set<T> ancestors = getAncestors(from);
+		Set<Node> ancestors = getAncestors(from);
 		Preconditions.checkArgument(
-			! ancestors.contains(to),
+			! ancestors.contains(toNode),
 			"loop detected. found child in ancestors: %s\n\t%s",
 			to,
 			ancestors);
@@ -91,7 +113,7 @@ public class Dag<T> {
 			roots.add(toNode);
 	}
 	
-	public Node removeNode(T value) {
+	public void removeNode(T value) {
 		Node node = nodes.remove(value);
 		Preconditions.checkArgument(
 			null != node,
@@ -105,30 +127,31 @@ public class Dag<T> {
 		
 		roots.remove(value);
 		
-		return node;
+//		return node;
 	}
 	
-	private Node getNode(T value) {
-		Node node = nodes.get(value);
-		Preconditions.checkArgument(
-			null != node,
-			"node not found: %s",
-			value);
-		return node;
-	}
-	private void getAncestorsRecursive(Node node, Set<T> ancestors) {
-		if (ancestors.add(node.value))
+	private void getAncestorsRecursive(Node node, Set<Node> ancestors) {
+		if (ancestors.add(node))
 			for (Node from : node.incoming.keySet())
 				getAncestorsRecursive(from, ancestors);
 	}
-	private void getDescendantsRecursive(Node node, Set<T> descendants) {
-		if (descendants.add(node.value))
+	private void getDescendantsRecursive(Node node, Set<Node> descendants) {
+		if (descendants.add(node))
 			for (Node from : node.outgoing.keySet())
 				getDescendantsRecursive(from, descendants);
 	}
 	
+	final Function<Node, T> getNodeValue = new Function<Node, T>() {
+		@Override
+		public T apply(Dag<T>.Node node) {
+			return node.value;
+		}
+	};
+	
+	/** XXX TODO implement Comparable! */
 	public class Node {
 		private final Dag<T> dag = Dag.this;
+		
 		
 		public final T value;
 		
@@ -171,6 +194,7 @@ public class Dag<T> {
 	
 	/** this class exists solely to encapsulate edge metadata */
 	private class Edge {
+		@SuppressWarnings("unused")
 		final Node
 			from,
 			to;
